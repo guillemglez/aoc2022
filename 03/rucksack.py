cac8dcae9701e3e2dfb374c1e21ed0df22401c0a
guillemglez <guillemglez@gmail.com>
2022-12-07 18:24:17 +0100

    Day 3, part 2
    kudos to @davidbeig

--- 03/rucksack.py
+++ 03/rucksack.py
@@ -13,6 +13,7 @@ def priority(char: str) -> int:
 
 
 def rucksack(input: Path) -> None:
+    uniques: list[set[str]] = []
     priorities: list[int] = []
 
     with input.open("r") as f:
@@ -21,13 +22,33 @@ def rucksack(input: Path) -> None:
             assert len(line) % 2 == 0
             nb_items = len(line) // 2
 
+            uniques.append(set(line))
             items = line[:nb_items], line[nb_items:]
             unique = set(items[0]), set(items[1])
             for item in unique[0]:
                 if item in unique[1]:
                     priorities.append(priority(item))
 
+    stickers: list[str] = []
+    for group in range(len(uniques) // 3):
+        rucksacks = uniques[group * 3 : group * 3 + 3]
+        content = ["".join(list(items)) for items in rucksacks]
+        overview: dict[str, int] = {}
+        for item in "".join(content):
+            if item not in overview.keys():
+                overview[item] = 1
+            else:
+                overview[item] += 1
+
+        for item, times in overview.items():
+            if times == 3:
+                stickers.append(item)
+                break
+
     print(f"The sum of priorities for the repeated items is {sum(priorities)}")
+    print(
+        f"The sum of priorities for the stickers is {sum([priority(s) for s in stickers])}"
+    )
 
 
 if __name__ == "__main__":
