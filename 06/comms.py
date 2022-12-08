35132ad17db9d117ae11b34b617775f20389921b
guillemglez <guillemglez@gmail.com>
2022-12-09 00:23:57 +0100

    Day 6, part 2

--- 06/comms.py
+++ 06/comms.py
@@ -2,25 +2,29 @@ from pathlib import Path
 from typing import Final
 
 
-def comms(input: Path) -> None:
-    stream: Final = input.open("r").readlines()[0].strip()
-
+def marker(stream: str, length: int) -> int:
     cursor = 0
     packet: list[str] = []
     while True:
         char = stream[cursor]
         cursor += 1
 
-        if len(packet) < 4:
+        if len(packet) < length:
             packet.append(char)
             continue
 
         packet.pop(0)
         packet.append(char)
 
-        if len(set(packet)) == 4:
-            print(f"Start-of-packet is found at {cursor}")
-            break
+        if len(set(packet)) == length:
+            return cursor
+
+
+def comms(input: Path) -> None:
+    stream: Final = input.open("r").readlines()[0].strip()
+
+    print(f"Start-of-packet is found at {marker(stream, 4)} when its length is 4")
+    print(f"Start-of-packet is found at {marker(stream, 14)} when its length is 14")
 
 
 if __name__ == "__main__":
