f68279c7ef7080442f3ed1ebe8e972b5ccf31b32
guillemglez <guillemglez@gmail.com>
2022-12-08 23:56:08 +0100

    Day 5, part 2

--- 05/supply.py
+++ 05/supply.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import copy
 
 
 def supply(input: Path) -> None:
@@ -35,14 +36,25 @@ def supply(input: Path) -> None:
             split = line_raw.split()
             ops.append((int(split[1]), int(split[3]), int(split[5])))
 
+    # Create copy for CM 9001
+    supplies_cm_9001 = copy.deepcopy(supplies)
+
     # perform processing
     for op in ops:
         times, frm, to = op
         for n in range(times):
             supplies[to - 1].append(supplies[frm - 1].pop())
+            supplies_cm_9001[to - 1].append(supplies_cm_9001[frm - 1][-times + n])
+
+        # empty moved crates in CM 9001 version
+        [supplies_cm_9001[frm - 1].pop() for n in range(times)]
 
     top_crates = [stack.pop() for stack in supplies]
+    top_crates_cm_9001 = [stack.pop() for stack in supplies_cm_9001]
     print(f"Give the Elves the message {''.join(top_crates)}")
+    print(
+        f"Give the Elves the message {''.join(top_crates_cm_9001)} as they use the CrateMover 9001"
+    )
 
 
 if __name__ == "__main__":
